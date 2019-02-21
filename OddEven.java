bf611016ac5f9b5c3810f8de9290138f07d283e0
ilakkiyasiva <47548837+ilakkiyasiva@users.noreply.github.com>
2019-02-21 00:55:01 -0800

    Create OddEven.java

---
+++ OddEven.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class OddEven{
+public static void main(String[] qwe){
+Scanner scan=new Scanner(System.in);
+System.out.println("enter the no:");
+int a=scan.nextInt();
+if(a%2==0){
+System.out.println(a+"is even");
+}
+else{
+System.out.println(a+"is odd");
+}
+}
+}
