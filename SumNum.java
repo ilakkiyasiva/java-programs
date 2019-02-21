03115c254fe17f4488a0dff70ea0524128a8a2e5
ilakkiyasiva <47548837+ilakkiyasiva@users.noreply.github.com>
2019-02-21 01:04:43 -0800

    Create SumNum.java

---
+++ SumNum.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class SumNum{
+public static void main(String[] qwe){
+Scanner scan=new Scanner(System.in);
+int N=scan.nextInt();
+int sum=0,i=1;
+while(i<=N){
+sum=sum+i;
+i++;
+}
+System.out.println(sum);
+}
+}
