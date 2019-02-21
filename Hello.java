5d1a5725084367295012e57f5b821a7f0d33643c
ilakkiyasiva <47548837+ilakkiyasiva@users.noreply.github.com>
2019-02-21 01:02:53 -0800

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Hello{
+public static void main(String[] qwe){
+Scanner scan=new Scanner(System.in);
+int n=scan.nextInt();
+int x=1;
+while(x<=n){
+System.out.println("Hello");
+x++;
+}
+}
+}
